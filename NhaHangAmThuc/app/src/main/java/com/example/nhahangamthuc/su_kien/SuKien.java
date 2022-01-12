3a63e05afbbe989a180648c51ea3783493da3ba4
nganhtu <nganhtu0712@gmail.com>
2022-01-12 10:27:17 +0700

    create SuKien

--- NhaHangAmThuc/app/src/main/java/com/example/nhahangamthuc/su_kien/SuKien.java
+++ NhaHangAmThuc/app/src/main/java/com/example/nhahangamthuc/su_kien/SuKien.java
@@ -1,4 +1,55 @@
 package com.example.nhahangamthuc.su_kien;
 
+import com.example.nhahangamthuc.mon_an.MonAn;
+
+import java.util.Date;
+import java.util.List;
+
 public class SuKien {
+    private int id;
+    private String ten;
+    private Date ngay;
+    private List<MonAn> monAnTangKemList;
+
+    public SuKien() {
+    }
+
+    public SuKien(int id, String ten, Date ngay, List<MonAn> monAnTangKemList) {
+        this.id = id;
+        this.ten = ten;
+        this.ngay = ngay;
+        this.monAnTangKemList = monAnTangKemList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public Date getNgay() {
+        return ngay;
+    }
+
+    public void setNgay(Date ngay) {
+        this.ngay = ngay;
+    }
+
+    public List<MonAn> getMonAnTangKemList() {
+        return monAnTangKemList;
+    }
+
+    public void setMonAnTangKemList(List<MonAn> monAnTangKemList) {
+        this.monAnTangKemList = monAnTangKemList;
+    }
 }
